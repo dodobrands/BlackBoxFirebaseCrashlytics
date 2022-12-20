1911f621550f0121db88be69d69777885eafe69b
Aleksey Berezka <a.berezka@dodopizza.com>
2022-12-20 20:28:42 +0700

    Added support foe watchOS and tvOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,9 @@ let package = Package(
     name: packageName,
     platforms: [
         .iOS(.v12),
-        .macOS(.v10_14)
+        .macOS(.v10_14),
+        .tvOS(.v12),
+        .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -37,15 +39,15 @@ let package = Package(
         .target(
             name: targetName,
             dependencies: [
-                "BlackBox",
-                "FirebaseCrashlytics"
+                .product(name: "BlackBox", package: "BlackBox-ios"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk")
             ]
         ),
         .testTarget(
             name: testTargetName,
             dependencies: [
                 .targetItem(name: targetName, condition: nil),
-                "BlackBox"
+                .product(name: "BlackBox", package: "BlackBox-ios")
             ]
         ),
     ],
