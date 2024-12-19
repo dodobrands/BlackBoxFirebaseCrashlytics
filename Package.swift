502d6b9c07a932bce39720304c276ac453f22a23
Aleksey Berezka <a.berezka@dodobrands.io>
2024-12-19 13:51:19 +0500

    Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -50,6 +50,5 @@ let package = Package(
                 "BlackBox"
             ]
         ),
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
